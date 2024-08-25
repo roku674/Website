d845575239ba2cdfc73a836b1c21fa7bc0f12191
Alexander Fields <26679979+roku674@users.noreply.github.com>
2024-08-25 16:07:43 -0400

    Update Projects.js

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -8,12 +8,39 @@ const Projects = () => {
       <Box mt={4}>
         <Typography variant="h4" gutterBottom>Projects</Typography>
         <List>
+          {/* Kingdom Of God Project */}
           <ListItem>
-            <ListItemText primary="Kingdom Of God - A website to connect people worldwide in a social media platform centered around philosophy and strong community." />
+            <ListItemText
+              primary={
+                <>
+                  Kingdom Of God - A website to connect people worldwide in a social media platform centered around philosophy and strong community.{' '}
+                  <a href="https://www.kingdomofgod.online" target="_blank" rel="noopener noreferrer">
+                    Visit Website
+                  </a>{' '}
+                  |{' '}
+                  <a href="https://github.com/kingdomofgod-online/kog-website" target="_blank" rel="noopener noreferrer">
+                    GitHub Repository
+                  </a>
+                </>
+              }
+            />
           </ListItem>
+
+          {/* Ad Astra Imperium Project */}
           <ListItem>
-            <ListItemText primary="Ad Astra - Currently in progress." />
+            <ListItemText
+              primary={
+                <>
+                  Ad Astra Imperium - Video Game currently in progress.{' '}
+                  <a href="https://www.perilousgames.com/dashboard/games" target="_blank" rel="noopener noreferrer">
+                    Download Ad Astra
+                  </a>
+                </>
+              }
+            />
           </ListItem>
+
+          {/* Automated Stock Trading Bot Project */}
           <ListItem>
             <ListItemText primary="Automated Stock Trading Bot - Currently working on this project." />
           </ListItem>
