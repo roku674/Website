7a0f81b08b40b5d492d3ccdc399acb9d58e9a772
Alexander Fields <Roku674@gmail.com>
2025-03-10 13:32:19 -0400

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -16,23 +16,29 @@ const Navbar = () => {
       <ListItem button component={Link} to="/" onClick={handleDrawerToggle}>
         <ListItemText primary="Home" />
       </ListItem>
-      <ListItem button component={Link} to="/bots" onClick={handleDrawerToggle}>
-        <ListItemText primary="Bots" />
-      </ListItem>
-      <ListItem button component={Link} to="/contact" onClick={handleDrawerToggle}>
-        <ListItemText primary="Contact" />
-      </ListItem>
       <ListItem button component={Link} to="/experience" onClick={handleDrawerToggle}>
         <ListItemText primary="Experience" />
+      </ListItem>
+	  <ListItem button component={Link} to="/projects" onClick={handleDrawerToggle}>
+        <ListItemText primary="Projects" />
+      </ListItem>
+	  <ListItem button component={Link} to="/tools" onClick={handleDrawerToggle}>
+        <ListItemText primary="Tools" />
+      </ListItem>
+	  <ListItem button component={Link} to="/bots" onClick={handleDrawerToggle}>
+        <ListItemText primary="Bots" />
+      </ListItem>
+	  <ListItem button component={Link} to="/products" onClick={handleDrawerToggle}>
+        <ListItemText primary="Products" />
+      </ListItem>
+	 <ListItem button component={Link} to="/services" onClick={handleDrawerToggle}>
+        <ListItemText primary="Services" />
       </ListItem>
       <ListItem button component={Link} to="/personality" onClick={handleDrawerToggle}>
         <ListItemText primary="Personality" />
       </ListItem>
-      <ListItem button component={Link} to="/projects" onClick={handleDrawerToggle}>
-        <ListItemText primary="Projects" />
-      </ListItem>
-      <ListItem button component={Link} to="/tools" onClick={handleDrawerToggle}>
-        <ListItemText primary="Tools" />
+      <ListItem button component={Link} to="/contact" onClick={handleDrawerToggle}>
+        <ListItemText primary="Contact" />
       </ListItem>
     </List>
   );
@@ -46,12 +52,14 @@ const Navbar = () => {
           </Typography>
           <div className="navbar-desktop">
             <Button color="inherit" component={Link} to="/">Home</Button>
-            <Button color="inherit" component={Link} to="/bots">Bots</Button>
-            <Button color="inherit" component={Link} to="/contact">Contact</Button>
             <Button color="inherit" component={Link} to="/experience">Experience</Button>
-            <Button color="inherit" component={Link} to="/personality">Personality</Button>
             <Button color="inherit" component={Link} to="/projects">Projects</Button>
             <Button color="inherit" component={Link} to="/tools">Tools</Button>
+			<Button color="inherit" component={Link} to="/bots">Bots</Button>
+			<Button color="inherit" component={Link} to="/products">Products</Button>
+			<Button color="inherit" component={Link} to="/services">Services</Button>
+            <Button color="inherit" component={Link} to="/personality">Personality</Button>			
+            <Button color="inherit" component={Link} to="/contact">Contact</Button>
           </div>
           <IconButton 
             color="inherit" 
