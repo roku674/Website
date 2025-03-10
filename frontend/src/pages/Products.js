c3c176aa89da1fdc4bdd9d802f5d8b9c1149e8d5
Alexander Fields <Roku674@gmail.com>
2025-03-10 13:58:53 -0400

    Update Products.js

--- frontend/src/pages/Products.js
+++ frontend/src/pages/Products.js
@@ -5,7 +5,14 @@ import { Container, Typography, Box } from '@mui/material';
 const Products = () => {
   return (
     <Container>
-      <Box mt={4} className="bubble">
+      <Box 
+        mt={4} 
+        className="bubble"
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        textAlign="center"
+      >
         <Typography variant="h4" gutterBottom>Products (None Yet)</Typography>
       </Box>
     </Container>
