13b42215e67d133f177d0ae62b7a4b12e1996427
Alexander Fields <Roku674@gmail.com>
2025-03-10 23:40:20 -0400

    Update Projects.js

--- frontend/src/pages/Projects.js
+++ frontend/src/pages/Projects.js
@@ -19,41 +19,11 @@ const Projects = () => {
         flexDirection="column"
         alignItems="center"
         textAlign="center"
-      >        {/* Apply the bubble class here */}
+      >        
         <Typography variant="h4" gutterBottom>
           Projects
         </Typography>
         <List>
-          {/* Kingdom Of God Project 
-          <ListItem>
-            <ListItemText
-              primary={
-                <>
-                  Kingdom Of God - A website to connect people worldwide in a
-                  social media platform centered around philosophy and strong
-                  community.{" "}
-                  <a
-                    href="https://www.kingdomofgod.online"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    style={{ color: "#fde047" }}
-                  >
-                    Visit Website
-                  </a>{" "}
-                  |{" "}
-                  <a
-                    href="https://github.com/kingdomofgod-online/kog-website"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    style={{ color: "#fde047" }}
-                  >
-                    GitHub Repository
-                  </a>
-                </>
-              }
-            />
-          </ListItem>
-          */}
           {/* Ad Astra Imperium Project */}
           <ListItem>
             <ListItemText
@@ -100,11 +70,30 @@ const Projects = () => {
             />
           </ListItem>
 
+          {/* NuGet Profile */}
           <ListItem>
             <ListItemText
               primary={
                 <>
-                    If you have a project in mind and would like to enlist my help do not hesitate to reach out!
+                  My NuGet Packages - Check out my published NuGet packages.{" "}
+                  <a
+                    href="https://www.nuget.org/profiles/roku674"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    style={{ color: "#fde047" }}
+                  >
+                    Visit NuGet Profile
+                  </a>
+                </>
+              }
+            />
+          </ListItem>
+
+          <ListItem>
+            <ListItemText
+              primary={
+                <>
+                  If you have a project in mind and would like to enlist my help, do not hesitate to reach out!
                 </>
               }
             />
