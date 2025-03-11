14d4641438f82141af3813c531841775bb2af167
Alexander Fields <Roku674@gmail.com>
2025-03-10 23:56:48 -0400

    Update Projects.js

--- frontend/src/pages/Projects.js
+++ frontend/src/pages/Projects.js
@@ -24,6 +24,36 @@ const Projects = () => {
           Projects
         </Typography>
         <List>
+          {/* Kingdom Of God Project */}
+          <ListItem>
+            <ListItemText
+              primary={
+                <>
+                  Kingdom Of God - A website to connect people worldwide in a
+                  social media platform centered around philosophy and strong
+                  community.{" "}
+                  <a
+                    href="https://www.kingdomofgod.online"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    style={{ color: "#fde047" }}
+                  >
+                    Visit Website
+                  </a>{" "}
+                  |{" "}
+                  <a
+                    href="https://github.com/kingdomofgod-online/kog-website"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    style={{ color: "#fde047" }}
+                  >
+                    GitHub Repository
+                  </a>
+                </>
+              }
+            />
+          </ListItem>
+
           {/* Ad Astra Imperium Project */}
           <ListItem>
             <ListItemText
